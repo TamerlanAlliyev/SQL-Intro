cf02a92e41263aba80399ac73ce922aba0b9044c
Tamerlan Aliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2024-01-24 12:06:30 +0400

    Up

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,120 +1,52 @@
---CREATE DATABASE Students
+CREATE DATABASE Academy
 
---DROP DATABASE Students
+CREATE TABLE Students
+(
+	Id int identity primary key,
+	[Name] nvarchar(20),
+	Surname nvarchar(20),
+	Age tinyint,
+	[Group] nvarchar(4)
+)
 
+INSERT INTO Students VALUES
+(N'Tamerlan',N'Əliyev',25,'P237'),
+(N'Bürhannəddin',N'Mirsadiqzadə',22,'P237'),
+(N'Murad',N'Aydinov',19,'P237'),
+(N'Kamran',N'Həsənov',20,'P237'),
+(N'Toğrul',N'Hüseynov',22,'P237')
 
+INSERT INTO Students ([Name], Surname, Age, [Group])
+VALUES 
+    (N'Nihad', N'Tağıyev', 20, 'P237')
 
-
---CREATE DATABASE Academy
-
---CREATE TABLE student (
---	[Name] nvarchar(20),
---	Surname nvarchar(20),
---	Age tinyint,
---	[Group] nvarchar(5)
---);
-
---CREATE TABLE teacher (
---	[Name] nvarchar(20),
---	Surname nvarchar(20),
---	[Group] nvarchar(5)
---);
-
---DROP DATABASE Academy
-
-
-
-
-
-
-
---CREATE DATABASE Academy
-
---CREATE TABLE Students (
--- id int primary key,
--- [Name] nvarchar(20) not null,
--- Surname nvarchar(20) not null,
--- Age tinyint,
--- [Group] nvarchar(5)
---);
-
-
---CREATE TABLE Teacher (
--- id int primary key,
--- [Name] nvarchar(20) not null,
--- Surname nvarchar(20) not null,
--- [Group] nvarchar(5)
---);
-
-
-
---insert into Students(id,[Name],Surname,Age,[Group])
---values(1,'Tamerlan','Aliyev',25,'P237')
-
---SELECT * FROM Students
-
---SELECT *
---FROM Students
---WHERE [Name] = 'Tamerlan';
-
---insert into Students(id,[Name],Surname,Age,[Group])
---values(2,'Burhan','Mirsadiqzade',22,'P237')
-
-insert into Students(id,[Name],Surname,Age,[Group])
-values(3,'Murad','Aydinov',20,'P237')
-
-
---DELETE FROM Students
---WHERE id = 3
-
---SELECT * FROM Students
-
-
---insert into Students(id,[Name],Surname,Age,[Group])
---values(3,'Murad','Aydinov',20,'P237')
-
---insert into Students(id,[Name],Surname,Age,[Group])
---values(4,'Kamran','Hesenov',20,'P237')
-
---insert into Students(id,[Name],Surname,Age,[Group])
---values(3,'Nihat','Tagizade',20,'P237')
-
---insert into Students(id,[Name],Surname,Age,[Group])
---values(5,'Murad','Aydinov',20,'P237')
-
-
-
-
-
-
-INSERT INTO Teacher (id, [Name], Surname, [Group])
-VALUES (1, 'Ahad', 'Tagiyev', 'P237');
-
-INSERT INTO Teacher (id, [Name], Surname, [Group])
-VALUES (2, 'Huseyn', 'Mirzayev', 'P237');
-
-INSERT INTO Teacher (id, [Name], Surname, [Group])
-VALUES (3, 'Kamran', 'Cebiyev', 'P237');
-
-
-INSERT INTO Teacher (id, [Name], Surname, [Group])
-VALUES (4, 'Cosqun', 'Huseynov', 'P237');
-
-UPDATE Teacher
-SET [Group] = 'P337'
-WHERE [Group] = 'P237';
-
-UPDATE Teacher
-SET [Group] = 'P237'
-WHERE [Group] = 'P337';
-
-SELECT * FROM Teacher
-
-UPDATE Students
-SET [Name] = 'Huseyn'
-WHERE id = 4;
+INSERT INTO Students ([Name], Surname, Age, [Group])
+VALUES 
+    (N'Ramazan', N'Hüseynov', 15, 'P237')
 
 SELECT * FROM Students
 
-DELETE Students 
-WHERE id = 4;
+
+
+CREATE TABLE Teachers
+(
+	Id int identity primary key,
+	[Name] nvarchar(20),
+	Surname nvarchar(20),
+	[Group] nvarchar(4)
+)
+
+INSERT INTO Teachers VALUES
+(N'Əhəd',N'Tağıyev','P237'),
+(N'Hüseyn',N'Mirzəyev','P237'),
+(N'Kamran',N'Cəbiyev','P237')
+
+SELECT *FROM Teachers
+
+
+
+DELETE FROM Students WHERE Id = 6
+
+UPDATE Teachers
+SET [Group] = 'P137'
+WHERE [Name] = 'Kamran'
